129f30337ea9fa7b5181e9d6315c5ddcb59c5657
Robison Machado <robisonpmachado@gmail.com>
2022-08-24 22:47:33 -0300

    adicionado propriedade idade

--- src/curso/java/alex/Pessoa.java
+++ src/curso/java/alex/Pessoa.java
@@ -3,7 +3,16 @@ package curso.java.alex;
 public class Pessoa {
 	private String nome;
 	private String sobrenome;
+	private int idade;
 	
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
 	public String getNome() {
 		return nome;
 	}
